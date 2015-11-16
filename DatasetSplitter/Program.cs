8bc04f653b61290be0f121a69bd2fde99b81e65e
Kirill Makhonin <kroks.rus@gmail.com>
2015-11-16 15:30:23 +0300

    Added date splitter

---
+++ DatasetSplitter/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatasetSplitter
+{
+    class Program
+    {
+
+        public class DatasetItem
+        {
+            public int user;
+            public int item;
+            public int mark;
+
+            public DatasetItem(string baseLine)
+            {
+                string[] words = baseLine.Split(',');
+                if (words.Length < 3) return;
+
+                user = int.Parse(words[0]);
+                item = int.Parse(words[1]);
+                mark = Math.Min(int.Parse(words[2]), 5);
+            }
+
+            public override string ToString()
+            {
+                return String.Format("{0},{1},{2}", user, item, mark);
+            }
+
+            public bool IdsBetween(int a, int b)
+            {
+                return user >= a && user < b && item >= a && item < b && mark > 0;
+            }
+            
+        }
+
+
+        private string _baseSaveFile;
+        private List<DatasetItem> _items;
+        private int _idsInDataset;
+
+        Program(string datasetFile, int idsInDataset)
+        {
+            _baseSaveFile = datasetFile.EndsWith(".csv") ? datasetFile.Substring(0, datasetFile.Length - 3) : datasetFile;
+            _idsInDataset = idsInDataset;
+            _items = new List<DatasetItem>();
+
+            if (!File.Exists(datasetFile) || idsInDataset < 1)
+                return;
+
+            using (TextReader reader = File.OpenText(datasetFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    _items.Add(new DatasetItem(line));
+                
+            }
+
+
+
+        }
+
+
+        public bool Save()
+        {
+            int maxId = _items.Max(m => Math.Max(m.user, m.item));
+            int countOfChunks = (int)Math.Ceiling(maxId*1.0/_idsInDataset);
+
+
+            for (int i = 0; i < countOfChunks; i++)
+                File.WriteAllText(_baseSaveFile + i.ToString() + ".csv",
+                    string.Join("\n", _items.Where(item => item.IdsBetween(i*_idsInDataset, (i + 1)*_idsInDataset))
+                        .Select(m => m.ToString())));
+
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Call this program with 2 arguments: <dataset_file> and <ids_in_datasets>");
+                return;
+            }
+
+            Program p = new Program(args[0], Int32.Parse(args[1]));
+
+            
+            p.Save();
+
+            Console.WriteLine("Okay");
+
+        }
+    }
+}
